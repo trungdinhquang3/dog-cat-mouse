5e116bb2a75dbd1486bb5a0590ec54857a52bd4d
Trung <trung@coder.tokyo>
2020-02-27 10:09:50 +0700

    Add sayHi method for chicken

--- chicken.js
+++ chicken.js
@@ -1,7 +1,16 @@
-function Chicken() {
+var chalk = require('chalk');
+
+function Chicken(name) {
+	this.name = name;
 	this.stomach = [];
 }
 
 Chicken.prototype.eat = function (cat) {
 	this.stomach.push(cat);
 }
+
+Chicken.prototype.sayHi = function () {
+	console.log('Hi! My name is ' + chalk.blue(this.name));
+}
+
+module.exports = Chicken;
---
+++ index.js
@@ -0,0 +1,5 @@
+var Chicken = require('./Chicken');
+
+var chicken = new Chicken('Cock');
+
+chicken.sayHi();
